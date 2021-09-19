07ac89313bec0a8efdce874285fe8627203cc562
hirokoooomori <hirouta0408@gmail.com>
2021-09-19 09:55:48 +0000

    mypage_shop

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,7 +4,8 @@ class Public::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
     @comments = @customer.comments.order("created_at DESC")
     #favorite = favorite.shop.id
-    @favorites = @customer.favorite
+    @favorites = @customer.favorites
+    @candidates = @customer.candidates
     #@favorites = Favorite.where(shop_id: @shop.id)
     #@favorites_shop = Customer.find_by(id: @favorites.shop_id)
   end
